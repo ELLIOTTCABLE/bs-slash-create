bb07083ec1ee4a1c4b8168b1c49fcbfeb58186dc
ELLIOTTCABLE <git@ell.io>
2021-02-27 21:02:56 -0600

    First commit messages are always rather pointless.

---
+++ src/SlashCreate.ml
@@ -0,0 +1,63 @@
+type imageFormat = [ `jpg | `jpeg | `png | `webp | `gif ]
+
+type requireAllOptions
+
+(* FIXME: functional `filter` argument NYI *)
+(* FIXME: type the `resolve` argument *)
+external requireAllOptions :
+  dirname:string ->
+  ?excludeDirs:Js.Re.t ->
+  ?filter:Js.Re.t ->
+  ?map:(name:string -> path:string -> string) ->
+  ?recursive:bool ->
+  ?resolve:(< .. > Js.t -> < .. > Js.t) ->
+  unit ->
+  requireAllOptions = ""
+  [@@bs.obj]
+
+type syncCommandOptions
+
+external syncCommandOptions :
+  ?deleteCommands:bool ->
+  ?skipGuildErrors:bool ->
+  ?syncGuilds:bool ->
+  unit ->
+  syncCommandOptions = ""
+  [@@bs.obj]
+
+module SlashCreator = struct
+  type t
+
+  type options
+
+  external options :
+    applicationID:string ->
+    ?publicKey:string ->
+    ?token:string ->
+    ?serverHost:string ->
+    ?serverPort:int ->
+    ?endpointPath:string ->
+    ?autoAcknowledgeSource:bool ->
+    ?defaultImageFormat:imageFormat ->
+    ?defaultImageSize:int ->
+    ?latencyThreshold:int ->
+    ?maxSignatureTimestamp:int ->
+    ?ratelimiterOffset:int ->
+    ?requestTimeout:int ->
+    ?unknownCommandResponse:bool ->
+    unit ->
+    options = ""
+    [@@bs.obj]
+
+  external createWith : options -> t = "SlashCreator"
+    [@@bs.new] [@@bs.module "slash-create"]
+
+  external commandsPath : t -> string option = "" [@@bs.get]
+
+  external registerCommandsInPath : string -> t = "registerCommandsIn" [@@bs.send.pipe: t]
+
+  external registerCommandsIn : requireAllOptions -> t = "registerCommandsIn"
+    [@@bs.send.pipe: t]
+
+  external syncCommands : ?opts:syncCommandOptions -> unit -> t = "" [@@bs.send.pipe: t]
+end
